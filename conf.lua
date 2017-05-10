5f78388f90f243b47253774e46d64abd78bd0e05
gsom <zhelneen@yandex.ru>
2017-05-11 04:33:38 +0500

    app conf

---
+++ conf.lua
@@ -0,0 +1,21 @@
+function love.conf(t)
+    t.identity = "chop"
+	t.author = "Zhelnin Evgeniy"
+    t.console = false
+    t.window.title = "Chopper Duel"
+    t.window.icon = nil
+    t.window.width = 320*2
+    t.window.height = 200*2
+    t.window.borderless = false
+    t.window.resizable = false
+    t.window.minwidth = 1
+    t.window.minheight = 1
+    t.window.fullscreen = false
+    t.window.fullscreentype = "desktop"
+    t.window.vsync = true
+    t.window.msaa = 0
+    t.window.display = 1
+    t.window.highdpi = false
+    t.window.x = nil
+    t.window.y = nil
+end
